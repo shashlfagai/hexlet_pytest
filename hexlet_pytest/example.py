27c778d06e1f8d2beded1f861b09e7874110dab8
Loginov <itsmeegorloginov@gmail.com>
2023-06-22 13:49:46 +0300

    add workflow

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,15 @@
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
     return string[::-1]
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
